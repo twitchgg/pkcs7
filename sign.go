26528dd2f0f5c17d2e37f46bcacbe9209c6c9034
Paul van Brouwershaven <vanbroup@users.noreply.github.com>
2017-07-19 21:55:25 +0200

    Revert "Add unsigned attributes"

--- sign.go
+++ sign.go
@@ -41,7 +41,6 @@ func NewSignedData(data []byte) (*SignedData, error) {
 // SignerInfoConfig are optional values to include when adding a signer
 type SignerInfoConfig struct {
 	ExtraSignedAttributes []Attribute
-	ExtraUnsignedAttributes []Attribute
 }
 
 type signedData struct {
@@ -143,14 +142,6 @@ func (sd *SignedData) AddSignerChain(ee *x509.Certificate, pkey crypto.PrivateKe
 	if err != nil {
 		return err
 	}
-	unsigned_attrs := &attributes{}
-	for _, attr := range config.ExtraUnsignedAttributes {
-		unsigned_attrs.Add(attr.Type, attr.Value)
-	}
-	finalUnsignedAttrs, err := unsigned_attrs.ForMarshalling()
-	if err != nil {
-		return err
-	}
 	signature, err := signAttributes(finalAttrs, pkey, hash)
 	if err != nil {
 		return err
@@ -170,7 +161,6 @@ func (sd *SignedData) AddSignerChain(ee *x509.Certificate, pkey crypto.PrivateKe
 	}
 	signer := signerInfo{
 		AuthenticatedAttributes:   finalAttrs,
-		UnauthenticatedAttributes: finalUnsignedAttrs,
 		DigestAlgorithm:           pkix.AlgorithmIdentifier{Algorithm: oidDigestAlg},
 		DigestEncryptionAlgorithm: pkix.AlgorithmIdentifier{Algorithm: oidEncryptionAlg},
 		IssuerAndSerialNumber:     ias,
